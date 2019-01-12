0f611568e74694e8e6f04b563f7a8eea5b5ce072
David Potter <dnpotter001@gmail.com>
2019-01-12 18:51:07 +0000

    restructed file system to prevent looping imports

--- mileage/routes.py
+++ mileage/routes.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, url_for, flash, redirect, request 
+from flask import render_template, url_for, flash, redirect, request
+from mileage import app
 from form import SetWorkoutForm
-import pyrow
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 workouts = [
   {
@@ -16,8 +14,6 @@ workouts = [
     'date': '12th Jan 2029'
   }
 ]
-
-
 
 @app.route("/")
 @app.route("/home")
@@ -46,15 +42,3 @@ def set_workout():
   pm5 = pyrow.pyrow(ergs[0])
   pm5.set_workout(distance=dist)
   return 'setting workout'
-
-if __name__ == '__main__':
-  app.run(debug=True)
-
-# ergs = list(pyrow.find())
-# if len(ergs) == 0:
-#     print('No ergs found')
-
-# erg = pyrow.pyrow(ergs[0])
-# print "Connected to erg."
-
-# erg.set_workout(distance=2000, split=100, pace=120)
