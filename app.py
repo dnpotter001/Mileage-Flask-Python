d37399f2ba0477325ee7f000577d252fe70fe7e4
David Potter <dnpotter001@gmail.com>
2019-01-12 13:30:55 +0000

    created form but getting errors from validate on submit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash,redirect
+from form import SetWorkoutForm
 import pyrow
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 workouts = [
   {
@@ -15,25 +17,39 @@ workouts = [
   }
 ]
 
+
+
+
+
 @app.route("/")
 @app.route("/home")
 @app.route("/feed")
 @app.route("/index")
 def home():
-    return render_template('index.html', posts=workouts, title="Your Feed")
+  return render_template('index.html', posts=workouts, title="Your Feed")
 
 @app.route("/about")
 def about():
-  return  render_template('about.html', title="About")
+  return render_template('about.html', title="About")
+
+@app.route('/erg', methods=['GET', 'POST'])
+def erg():
+  form = SetWorkoutForm()
+  if form.validate_on_submit():
+    ergs = list(pyrow.find())
+    pm5 = pyrow.pyrow(ergs[0])
+    pm5.set_workout(distance=form.distance.data)
+    return redirect(url_for('erg'))
+  return render_template('erg.html', title='Erg Control', form=form)
 
 if __name__ == '__main__':
   app.run(debug=True)
 
-ergs = list(pyrow.find())
-if len(ergs) == 0:
-    print('No ergs found')
+# ergs = list(pyrow.find())
+# if len(ergs) == 0:
+#     print('No ergs found')
 
-erg = pyrow.pyrow(ergs[0])
-print "Connected to erg."
+# erg = pyrow.pyrow(ergs[0])
+# print "Connected to erg."
 
-erg.set_workout(distance=2000, split=100, pace=120)
+# erg.set_workout(distance=2000, split=100, pace=120)
--- form.py
+++ form.py
@@ -5,18 +5,18 @@ from wtforms.validators import Length, DataRequired
 class SetWorkoutForm(FlaskForm):
   
   distance = IntegerField('distance',
-    validators=[DataRequired(), Length(min=1, max=5)])
+    validators=[Length(min=1, max=5)])
 
   hours = IntegerField('hours',
-    validators=[DataRequired(), Length(min=0, max=1)])
+    validators=[Length(min=0, max=1)])
   
   minutes = IntegerField('mintues',
-    validators=[DataRequired(), Length(min=0, max=2)])
+    validators=[Length(min=0, max=2)])
 
-  minutes = IntegerField('second',
-    validators=[DataRequired(), Length(min=0, max=2)])
+  seconds = IntegerField('seconds',
+    validators=[ Length(min=0, max=2)])
   
-  split = IntegerField('distance', 
-    validators=[DataRequired(), Length(min=1, max=3)])
+  split = IntegerField('split', 
+    validators=[Length(min=1, max=3)])
 
   submit = SubmitField('Set Workout')
