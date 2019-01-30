7445cefcc6e83f5ec62fafb47e63c846941de10a
David Potter <dnpotter001@gmail.com>
2019-01-30 15:25:52 +0000

    started creating the bar chart

--- mileage/static/js/svgCharts.js
+++ mileage/static/js/svgCharts.js
@@ -9,34 +9,34 @@ chars.forEach(c => {
 
 console.log(colours)
 
-let BarChart = function(area, intervals){
+let BarChart = function (area, intervals) {
 
-  this.createSVG = function(){
+  this.createSVG = function () {
     this.ns = 'http://www.w3.org/2000/svg'
     this.svg = document.createElementNS(this.ns, 'svg')
     this.svg.setAttributeNS(null, 'width', '100%')
     this.svg.setAttributeNS(null, 'height', '100%')
     area.appendChild(this.svg)
   }
-  
-  this.drawSeries = function(){
+
+  this.drawSeries = function () {
     let barCount = intervals.length
-    let barHeight = 100/barCount;
-    let maxValue= 0;
-  
-    if(barCount == 1){
+    let barHeight = 100 / barCount;
+    let maxValue = 0;
+
+    if (barCount == 1) {
       chartArea.innerHTML = "More intervals needed for bar chart. "
       return;
     }
     intervals.forEach(x => {
       let split = x[1].split(/./ && /:/)
       let totalTime = 0;
-      if (split.length == 2){
+      if (split.length == 2) {
         totalTime = (split[0] * 60) + parseFloat(split[1]);
       } else {
-        totalTime = (split[0]* 60 * 60) + (split[1] * 60) + parseFloat(split[2]);
+        totalTime = (split[0] * 60 * 60) + (split[1] * 60) + parseFloat(split[2]);
       }
-      if (totalTime > maxValue){
+      if (totalTime > maxValue) {
         maxValue = totalTime
       }
       x.push(totalTime);
@@ -49,23 +49,36 @@ let BarChart = function(area, intervals){
 
       let rect = document.createElementNS(this.ns, 'rect')
       rect.setAttributeNS(null, 'width', barInc * totalTime + "%")
-      rect.setAttributeNS(null, 'height', barHeight -2 + "%")
-      rect.setAttributeNS(null, 'y', (x[0] -1) * barHeight +2+ "%")
-      rect.setAttributeNS(null, 'fill', colours[x[0]%colours.length])
-  
+      rect.setAttributeNS(null, 'height', barHeight - 2 + "%")
+      rect.setAttributeNS(null, 'y', (x[0] - 1) * barHeight + 2 + "%")
+      rect.setAttributeNS(null, 'fill', colours[x[0] % colours.length])
+
       let label = document.createElementNS(this.ns, 'text')
-      label.setAttributeNS(null, 'y', (x[0]-0.5) * barHeight + 2.75 +  "%");
-      label.setAttributeNS(null, 'x', (barInc * totalTime)+ 1 + "%");
+      label.setAttributeNS(null, 'y', (x[0] - 0.5) * barHeight + 2.75 + "%");
+      label.setAttributeNS(null, 'x', (barInc * totalTime) + 1 + "%");
       label.innerHTML = x[1]
-  
+
       let g = document.createElementNS(this.ns, 'g');
       g.appendChild(rect)
       g.appendChild(label)
-      this.svg.appendChild(g) 
-     
+      this.svg.appendChild(g)
+
     })
     area.appendChild(this.svg)
+  }
+}
 
+let LineGraph = function (area) {
 
+  this.createSVG = function () {
+    this.ns = 'http://www.w3.org/2000/svg'
+    this.svg = document.createElementNS(this.ns, 'svg')
+    this.svg.setAttributeNS(null, 'width', '100%')
+    this.svg.setAttributeNS(null, 'height', '100%')
+    area.appendChild(this.svg)
+  }
+
+  this.drawSeries = function(intervals, series) {
+    return;
   }
 }
